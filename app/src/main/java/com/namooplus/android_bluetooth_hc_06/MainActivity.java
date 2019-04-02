256bf7db324f92473f85e9b39ceba28cadf07cdb
junghyen2003 <junghyen2003@gmail.com>
2019-04-02 16:05:35 +0900

    블루투스 기기 검색 메뉴 생성

--- app/src/main/java/com/namooplus/android_bluetooth_hc_06/MainActivity.java
+++ app/src/main/java/com/namooplus/android_bluetooth_hc_06/MainActivity.java
@@ -2,6 +2,10 @@ package com.namooplus.android_bluetooth_hc_06;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // 메뉴 추가
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bluetooth_search, menu);
+        return true;
+    }
+
+    // 메뉴 선택 시 동작
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.bluetooth_search_menu:
+                // 블루투스 기기 다이얼로그 생성
+                Toast.makeText(this, "블루투스 기기 다이얼로그 생성",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
